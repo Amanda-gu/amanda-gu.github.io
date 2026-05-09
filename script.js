e02303808692928135baeebde3d023e0959ebd66
Amanda-gu <guoj826@newschool.edu>
2026-05-08 23:04:47 -0400

    styled cursor lines and sq

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-
+//code taken from links, but instead of creating new shape, i defined the shapes in html and styled them in css, and then just move them with js
 window.addEventListener('mousemove', (e) => {
-  // 1.Create a new div everytime it moves so there is a trail
+ 
   	let hLine = document.getElementById('h-line')
     let vLine = document.getElementById('v-line')
+    let sq = document.getElementById('sq')
 
     hLine.style.left = `${e.pageX}px`
     hLine.style.top = `${e.pageY}px`
 
     vLine.style.left = `${e.pageX}px`
     vLine.style.top = `${e.pageY}px`
+
+    sq.style.left = `${e.pageX}px`
+    sq.style.top = `${e.pageY}px`
 });
